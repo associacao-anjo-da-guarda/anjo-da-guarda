47a39a856618ceba3667ca5f85f1949ff8bb5223
pmoreira <pmoreira@firenzze.com>
2020-06-03 20:48:53 -0300

    add home cms fields to test the netlify cms integration

--- store/index.js
+++ store/index.js
@@ -1,5 +1,6 @@
 export const state = () => ({
-    blogPosts: []
+    blogPosts: [],
+    home: []
 })
 
 export const mutations = {
@@ -15,11 +16,23 @@ export const actions = {
             false,
             /\.json$/
         )
+
+        // const homeFile = await import('~/assets/content/home.json')
+
+        // console.log('files', files.keys())
+        // console.log('files', files.keys())
+
         const blogPosts = files.keys().map((key) => {
             const res = files(key)
+
+            // console.log('files(key)', files(key))
+
             res.slug = key.slice(2, -5)
             return res
         })
+
+        // console.log('blogPosts', blogPosts)
+
         await commit('setBlogPosts', blogPosts)
     }
 }
