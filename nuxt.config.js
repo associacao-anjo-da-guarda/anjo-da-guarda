fa5e7cb41136de19b95d6c8054ec2bc8f705f6a6
pmoreira <pmoreira@firenzze.com>
2020-06-15 01:14:47 -0300

    add graphql file loader setup to webpack: https://github.com/nuxt/nuxt.js/issues/155

--- nuxt.config.js
+++ nuxt.config.js
@@ -169,6 +169,12 @@ export default {
         /*
          ** You can extend webpack config here
          */
-        extend (config, ctx) { }
+        extend (config, ctx) {
+            config.module.rules.push({
+                test: /\.(graphql|gql)$/,
+                exclude: /node_modules/,
+                loader: 'graphql-tag/loader'
+            })
+        }
     }
 }
