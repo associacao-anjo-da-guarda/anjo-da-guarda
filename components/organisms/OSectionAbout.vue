4e0ab6fcf89685d3d9acfdde4de52d5e58136398
pmoreira <pmoreira@firenzze.com>
2020-06-16 00:28:49 -0300

    integrate prismic to the home page

---
+++ components/organisms/OSectionAbout.vue
@@ -0,0 +1,51 @@
+<template>
+
+    <o-section class="o-section-about">
+        <o-wrapper
+            padding-section
+            centered-content
+            row-gap-normal
+            boxed
+        >
+
+            <a-title
+                tag="h2"
+                level2
+                v-html="$prismic.asText(sectionData.title)"
+            />
+
+            <a-text v-html="$prismic.asHtml(sectionData.text)" />
+
+            <a-button
+                primary
+                large
+                to="/quem-somos"
+            >
+                {{ $prismic.asText(sectionData.button_text) }}
+            </a-button>
+
+        </o-wrapper>
+    </o-section>
+
+</template>
+
+<script>
+export default {
+
+    name: 'OSectionAbout',
+
+    props: {
+        sectionData: {
+            type: Object,
+            required: true
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.o-section-about {
+    background-color: var(--color-secondary);
+}
+</style>
