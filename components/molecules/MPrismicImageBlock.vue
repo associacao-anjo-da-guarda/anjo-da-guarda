9a2d70d0508eb2000d71ef2a57aeeb256c6f5249
pmoreira <pmoreira@firenzze.com>
2020-06-18 15:48:32 -0300

    add template to the 'Pagina Simples' content type

---
+++ components/molecules/MPrismicImageBlock.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <a-image
+        class="m-prismic-image-block"
+        :src="componentData.image.url"
+        :alt="componentData.image.alt"
+    />
+
+</template>
+
+<script>
+export default {
+
+    props: {
+        componentData: {
+            type: Object,
+            required: true
+        }
+    }
+
+}
+</script>
