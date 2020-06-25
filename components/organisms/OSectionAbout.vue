4edbeb342563e910375b1189597840805c07457d
pmoreira <pmoreira@firenzze.com>
2020-06-25 17:10:47 -0300

    add video to the home about section

--- components/organisms/OSectionAbout.vue
+++ components/organisms/OSectionAbout.vue
@@ -16,6 +16,11 @@
 
             <a-text v-html="$prismic.asHtml(sectionData.text)" />
 
+            <div
+                class="a-embed"
+                v-html="sectionData.video.html"
+            />
+
             <a-button
                 primary
                 large
@@ -47,5 +52,47 @@ export default {
 <style scoped>
 .o-section-about {
     background-color: #f3f9d2;
+}
+
+.a-embed {
+    width: 100%;
+    max-width: 100%;
+    overflow: hidden;
+    padding-bottom: 56.25%;
+    position: relative;
+    height: 0;
+    border-radius: 10px;
+}
+@media screen and (min-width: 600px) {
+    .a-embed {
+        width: 480px;
+        max-width: 480px;
+        overflow: unset;
+        padding-bottom: 0;
+        position: unset;
+        height: 270px;
+    }
+}
+
+.a-embed >>> iframe,
+.a-embed >>> object,
+.a-embed >>> embed {
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+}
+
+@media screen and (min-width: 600px) {
+    .a-embed >>> iframe,
+    .a-embed >>> object,
+    .a-embed >>> embed {
+        left: unset;
+        top: unset;
+        width: 480px;
+        height: 270px;
+        position: unset;
+    }
 }
 </style>
