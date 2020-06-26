30b90ff215d7e7682ed04bc51abfb045585ddc5c
pmoreira <pmoreira@firenzze.com>
2020-06-26 13:50:49 -0300

    add featured image component

---
+++ components/organisms/OSectionFeaturedImage.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <o-section>
+
+        <o-wrapper v-if="featuredImage">
+
+            <a-image
+                width="100%"
+                height="50vh"
+                is-bg
+                :src="featuredImage.url"
+            />
+
+        </o-wrapper>
+
+        <o-wrapper
+            v-else
+            padding-section
+        />
+
+    </o-section>
+
+</template>
+
+<script>
+export default {
+
+    name: 'OSectionFeaturedImage',
+
+    props: {
+
+        featuredImage: {
+            type: Object,
+            default: () => { }
+        }
+    }
+
+}
+</script>
