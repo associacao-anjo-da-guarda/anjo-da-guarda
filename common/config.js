04fc69492407e4ba7603a3f9a1816c87ac9cf459
pmoreira <pmoreira@firenzze.com>
2020-08-26 19:26:36 -0300

    fix title address

--- common/config.js
+++ common/config.js
@@ -2,7 +2,7 @@ export default {
     app: {
         siteUrl: 'https://projetoanjodaguarda.org',
         siteName: 'Associação Anjo da Guarda',
-        siteTitle: 'Associação Anjo da Guarda | Ribeirão Preto, SP, Brasil',
+        siteTitle: 'Associação Anjo da Guarda | São José do Rio Preto, SP, Brasil',
         siteDescription: 'Ajudamos crianças e adolescentes a ter um futuro melhor. Seja nosso herói',
         siteSocialImage: '/social-share-image.png',
         twitterUser: ''
